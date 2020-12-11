5060436f2c1c2a923a5268f1159ca55eb0bdf931
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-12-11 18:22:06 +0100

    Refactor a bit day 11.

--- day11/Main.hs
+++ day11/Main.hs
@@ -1,13 +1,24 @@
 module Main where
 
 import Data.Maybe (catMaybes)
-import Data.Vector (Vector, fromList, (!), (!?), (//), imap)
+import Data.Vector (Vector, fromList, (!), (!?), imap)
 import qualified Data.Vector as V (sum)
 
 main :: IO ()
 main = do
-  map <- fromList . map fromList . lines <$> readFile "input.txt"
-  print $ solve <$> [golRule1, golRule2] <*> pure map
+    map <- fromList . map fromList . lines <$> readFile "input.txt"
+    print $ solve <$> [rule1, rule2] <*> pure map
+  where
+    rule1 = genRule 4 $ \getCell x y dx dy  -> getCell (x + dx) (y + dy)
+    rule2 = genRule 5 coFind
+      where
+        coFind getCell x' y' dx' dy' = go (x' + dx') (y' + dy') dx' dy'
+          where
+            go x y dx dy = do
+              c <- getCell x y
+              if c == 'L' || c == '#' then Just c else go (x + dx) (y + dy) dx dy
+
+type Rule = Int -> Int -> Vector (Vector Char) -> Char -> Char
 
 solve :: Rule -> Vector (Vector Char) -> Int
 solve rule = go
@@ -16,45 +27,24 @@ solve rule = go
       let newMap = mutateMap rule map
       in if newMap == map then countOccupied newMap else go newMap
     countOccupied = V.sum . fmap (V.sum . fmap isOccupied)
+    mutateMap rule map = imap mutateRow map
+      where
+        mutateRow y = imap (mutateCol y)
+        mutateCol y x = rule x y map
 
-mutateMap :: Rule -> Vector (Vector Char) -> Vector (Vector Char)
-mutateMap rule map = imap mutateRow map
-  where
-    mutateRow y = imap (mutateCol y)
-    mutateCol y x = rule x y map
-
-type Rule = Int -> Int -> Vector (Vector Char) -> Char -> Char
-
-golRule1 :: Rule
-golRule1 x y map cell =
+genRule :: Int -> ((Int -> Int -> Maybe Char) -> Int -> Int -> Int -> Int -> Maybe Char) -> Rule
+genRule overcrowded findCell x y map cell =
     case cell of
       'L' -> if adjacentSeats == 0 then '#' else 'L'
-      '#' -> if adjacentSeats >= 4 then 'L' else '#'
+      '#' -> if adjacentSeats >= overcrowded then 'L' else '#'
       _ -> cell
   where
     adjacentSeats = sum (catMaybes seats)
-    seats = [isOccupied <$> getCell (x + x') (y + y') | x' <- [-1..1], y' <- [-1..1], x' /= 0 || y' /= 0]
+    seats = [isOccupied <$> findCell getCell x y x' y' | x' <- [-1..1], y' <- [-1..1], x' /= 0 || y' /= 0]
     getCell :: Int -> Int -> Maybe Char
     getCell x y = do
       row <- map !? y
       row !? x
-
-golRule2 :: Rule
-golRule2 x y map cell =
-    case cell of
-      'L' -> if visibleSeats == 0 then '#' else 'L'
-      '#' -> if visibleSeats >= 5 then 'L' else '#'
-      _ -> cell
-  where
-    visibleSeats = sum (catMaybes seats)
-    seats = [isOccupied <$> coFind (x + dx, y + dy) (dx, dy) | dx <- [-1..1], dy <- [-1..1], dx /= 0 || dy /= 0]
-    getCell :: Int -> Int -> Maybe Char
-    getCell x y = do
-      row <- map !? y
-      row !? x
-    coFind (x, y) dir@(dx, dy) = do
-      c <- getCell x y
-      if c == 'L' || c == '#' then Just c else coFind (x + dx, y + dy) dir
 
 isOccupied :: Char -> Int
 isOccupied '#' = 1
